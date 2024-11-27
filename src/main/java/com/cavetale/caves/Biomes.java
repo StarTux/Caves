93af08f71f876b69dde0c024b45e17c6918aed1e
StarTux <ths.loy@gmail.com>
2024-11-27 15:16:39 +0100

    1.21.3

--- src/main/java/com/cavetale/caves/Biomes.java
+++ src/main/java/com/cavetale/caves/Biomes.java
@@ -1,7 +1,12 @@
 package com.cavetale.caves;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -15,7 +20,7 @@ import org.bukkit.block.Block;
 @RequiredArgsConstructor
 final class Biomes {
     private final Logger logger;
-    private final Map<Biome, Type> biomes = new EnumMap<>(Biome.class);
+    private final Map<Biome, Type> biomes = new HashMap<>();
     private final Map<Type, Set<Biome>> types = new EnumMap<>(Type.class);
     @Setter private boolean reportDuplicateBiomes = false;
 
@@ -76,5 +81,16 @@ final class Biomes {
 
     public Type of(Block block) {
         return biomes.get(block.getBiome());
+    }
+
+    public static void test() {
+        Biomes biomes = new Biomes(Logger.getGlobal());
+        biomes.setReportDuplicateBiomes(true);
+        biomes.load();
+        List<Biomes.Type> sorted = new ArrayList<>(biomes.getTypes().keySet());
+        Collections.sort(sorted, Comparator.comparing(t -> biomes.getTypes().get(t).size()));
+        for (Biomes.Type type : sorted) {
+            System.out.println(biomes.getTypes().get(type).size() + " " + type + " " + biomes.getTypes().get(type));
+        }
     }
 }
